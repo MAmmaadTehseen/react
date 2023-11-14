7bc2294233020aed1be7464caf1e0597aa48739d
Muhammad Ammaad Tehseen <97141225+MAmmaadTehseen@users.noreply.github.com>
2023-11-14 16:43:42 +0500

    14-11-23

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+<>
+<nav className="li">home</nav>
+<nav className="li">about</nav>
+<nav className="li">contact</nav>
+<nav className="li">about us</nav>
+
+
+
+
+</>
+
+  );
+}
+
+export default App;
