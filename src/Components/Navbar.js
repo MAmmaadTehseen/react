3161ce2f305d5ff92edc17c5327e66b42995edbf
Muhammad Ammaad Tehseen <97141225+MAmmaadTehseen@users.noreply.github.com>
2023-11-26 16:44:17 +0500

    26-11-23

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar(props) {
   return (
     <div>
       
-<nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} text-${props.mode=="light"?"grey":"light"}`} >
+<nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} text-${props.mode==="light"?"grey":"light"}`} >
   <div className="container-fluid">
     <a className="navbar-brand" href="">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,7 +16,7 @@ export default function Navbar(props) {
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className={`nav-link text-${props.mode=="light"?"grey":"light"}`} href="https://github.com/MAmmaadTehseen" target='_blank'>Contact Us</a>
+          <a className={`nav-link text-${props.mode==="light"?"grey":"light"}`} href="https://github.com/MAmmaadTehseen" target='_blank'>Contact Us</a>
         </li>
        
        
